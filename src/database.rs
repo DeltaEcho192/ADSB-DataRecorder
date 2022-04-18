d6525124648f36467f84422e4393e8afe3eefe18
DeltaEcho <anthony@durrer.co.za>
2022-04-18 17:59:29 +0200

    Working Database connect and insert of Flight data

---
+++ src/database.rs
@@ -0,0 +1,44 @@
+use mysql::*;
+use mysql::prelude::*;
+use chrono::prelude::*;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Flight {
+    pub hex: String,
+    pub flight: String,
+    pub lat: f64,
+    pub lon: f64,
+    pub altitude: i32,
+    pub track: i32,
+    pub speed: i32
+}
+
+pub fn insert(flight_data:Vec<Flight>)
+{
+    let opts = OptsBuilder::new()
+    .ip_or_hostname(Some("localhost"))
+    .user(Some("root"))
+    .pass(Some("xxmaster"))
+    .db_name(Some("flight_data"));
+    let pool = Pool::new(opts).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    conn.exec_batch(
+        r"INSERT INTO test_data (hex, flight, lat,lon,altitude,track,speed)
+          VALUES (:hex, :flight, :lat,:lon,:altitude,:track,:speed)",
+        flight_data.iter().map(|p| params! {
+            "hex" => &p.hex,
+            "flight" => &p.flight,
+            "lat" => p.lat,
+            "lon" => p.lon,
+            "altitude" => p.altitude,
+            "track" => p.track,
+            "speed" => p.speed
+        })
+    );
+} 
+
+pub fn hello_world()
+{
+    println!("{}", "Test");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 use reqwest::Error;
+mod database;
 
 #[derive(Deserialize, Debug)]
 struct Flight {
@@ -12,13 +13,30 @@ struct Flight {
     speed: i32
 }
 
+
+fn main()
+{
+    get_data();
+}
+
+
+
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn get_data() -> Result<(), Error>
+{
     let request_url = format!("http://192.168.1.119:8080/data.json");
     println!("{}", request_url);
     let response = reqwest::get(&request_url).await?;
 
-    let flight_data: Vec<Flight> = response.json().await?;
-    println!("{:?}", flight_data[1].flight);
+    let flight_data: Vec<database::Flight> = response.json().await?;
+    if(flight_data.len() > 0)
+    {
+        println!("{:?}", flight_data[0].flight);
+        database::insert(flight_data);
+    }
+    else{
+        println!("{}","No Planes in the air");
+    }
+    
     Ok(())
 }
